0350c9f00a6e88b96d2bde54a024a964c024956e
anondz <anondz619219@gmail.com>
2024-11-19 10:45:49 +0900

    first commit

---
+++ src/components/EditTopicForm.tsx
@@ -0,0 +1,56 @@
+'use client'
+
+import { updateTopic } from '@/actions/topicActions'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+
+interface EditTopicFormProps {
+  id: string
+  initialTitle: string
+  initialDescription: string
+}
+export default function EditTopicForm({
+  id,
+  initialTitle,
+  initialDescription,
+}: EditTopicFormProps) {
+  const [title, setTitle] = useState(initialTitle)
+  const [description, setDescription] = useState(initialDescription)
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    try {
+      await updateTopic(id, title, description)
+      router.push('/')
+    } catch (error) {
+      console.error('토픽 수정 중 오류:', error)
+      alert('토픽 수정 중 오류가 발생했습니다.')
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="토픽 제목"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        className="border border-slate-500 px-8 py-2"
+        placeholder="토픽 설명"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <button
+        type="submit"
+        className="bg-green-600 font-bold text-white py-3 px-6 w-fit"
+      >
+        수정하기
+      </button>
+    </form>
+  )
+}
